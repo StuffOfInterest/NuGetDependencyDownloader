575ee444a9f90b10b55a4e2839520ed24821ded9
Delbert Matlock <delbert.matlock@gmail.com>
2016-11-13 17:45:09 -0500

    Add project files.

---
+++ NugetDependencyDownloader/MainForm.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace NuGetDependencyDownloader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.textBoxPackage = new System.Windows.Forms.TextBox();
+            this.textBoxVersion = new System.Windows.Forms.TextBox();
+            this.textBoxActivity = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBoxPrerelease = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(498, 38);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(498, 67);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.EnabledChanged += new System.EventHandler(this.btnStop_EnabledChanged);
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // textBoxPackage
+            // 
+            this.textBoxPackage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxPackage.Location = new System.Drawing.Point(12, 48);
+            this.textBoxPackage.Name = "textBoxPackage";
+            this.textBoxPackage.Size = new System.Drawing.Size(211, 20);
+            this.textBoxPackage.TabIndex = 1;
+            // 
+            // textBoxVersion
+            // 
+            this.textBoxVersion.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxVersion.Location = new System.Drawing.Point(257, 48);
+            this.textBoxVersion.Name = "textBoxVersion";
+            this.textBoxVersion.Size = new System.Drawing.Size(136, 20);
+            this.textBoxVersion.TabIndex = 2;
+            // 
+            // textBoxActivity
+            // 
+            this.textBoxActivity.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxActivity.Location = new System.Drawing.Point(13, 109);
+            this.textBoxActivity.Multiline = true;
+            this.textBoxActivity.Name = "textBoxActivity";
+            this.textBoxActivity.ReadOnly = true;
+            this.textBoxActivity.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxActivity.Size = new System.Drawing.Size(560, 339);
+            this.textBoxActivity.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Package";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(257, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Version";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Activity";
+            // 
+            // checkBoxPrerelease
+            // 
+            this.checkBoxPrerelease.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBoxPrerelease.AutoSize = true;
+            this.checkBoxPrerelease.Location = new System.Drawing.Point(412, 48);
+            this.checkBoxPrerelease.Name = "checkBoxPrerelease";
+            this.checkBoxPrerelease.Size = new System.Drawing.Size(76, 17);
+            this.checkBoxPrerelease.TabIndex = 8;
+            this.checkBoxPrerelease.Text = "Prerelease";
+            this.checkBoxPrerelease.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.checkBoxPrerelease);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxActivity);
+            this.Controls.Add(this.textBoxVersion);
+            this.Controls.Add(this.textBoxPackage);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.MinimumSize = new System.Drawing.Size(600, 500);
+            this.Name = "MainForm";
+            this.Text = "NuGet Dependency Scraper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox textBoxPackage;
+        private System.Windows.Forms.TextBox textBoxVersion;
+        private System.Windows.Forms.TextBox textBoxActivity;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBoxPrerelease;
+    }
+}
+
---
+++ NugetDependencyDownloader/MainForm.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows.Forms;
+using NuGet;
+
+namespace NuGetDependencyDownloader
+{
+    public partial class MainForm : Form
+    {
+        delegate void SetStopCallback();
+
+        private bool StopRequested { get { return !btnStop.Enabled; } }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+
+            LaunchThread();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            EndWork();
+        }
+        private void btnStop_EnabledChanged(object sender, EventArgs e)
+        {
+            if (btnStop.Enabled == false)
+            {
+                btnStart.Enabled = true;
+            }
+        }
+
+        private void LaunchThread()
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.DoWork += new DoWorkEventHandler(bgWorker_DoWork);
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bgWorker_RunWorkerCompleted);
+            worker.RunWorkerAsync();
+        }
+
+        void bgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ProcessPackage();
+        }
+
+        void bgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            EndWork();
+        }
+
+        private void EndWork()
+        {
+            // Shut off stop button in a thread safe way
+            if (btnStop.InvokeRequired)
+            {
+                SetStopCallback d = new SetStopCallback(EndWork);
+                Invoke(d, new object[] { });
+            }
+            else
+            {
+                btnStop.Enabled = false;
+                btnStart.Enabled = true;
+            }
+        }
+
+        private void ProcessPackage()
+        {
+            CollectPackages();
+
+            if (StopRequested)
+            {
+                ShowConsole("Stopped.");
+                return;
+            }
+
+            ShowConsole(string.Format("{0} packages to download.", Utility.Packages.Count));
+
+            DownloadPackages();
+
+            if (StopRequested)
+            {
+                ShowConsole("Stopped.");
+                return;
+            }
+
+            ShowConsole("Done.");
+        }
+
+        private void CollectPackages()
+        {
+            Utility.Packages.Clear();
+
+            IPackage package;
+            if (string.IsNullOrWhiteSpace(textBoxVersion.Text))
+            {
+                package = Utility.GetLatestPackage(textBoxPackage.Text, checkBoxPrerelease.Checked);
+            }
+            else
+            {
+                var version = SemanticVersion.Parse(textBoxVersion.Text);
+                package = Utility.GetPackages(textBoxPackage.Text, checkBoxPrerelease.Checked)
+                    .Where(o => o.Version == version)
+                    .FirstOrDefault();
+            }
+
+            if (package == null)
+            {
+                ShowConsole("Package not found.");
+                return;
+            }
+
+            Utility.Packages.Add(package);
+            ShowConsole(package.GetFullName());
+            LoadDependencies(package);
+        }
+
+        private void LoadDependencies(IPackage package)
+        {
+            if (package.DependencySets != null)
+            {
+                var dependencies = package.DependencySets
+                    .SelectMany(o => o.Dependencies.Select(x => new Dependency { Id = x.Id, VersionSpec = x.VersionSpec }))
+                    .ToList();
+
+                foreach (var dependency in dependencies)
+                {
+                    if (StopRequested)
+                        return;
+
+                    IQueryable<IPackage> packages = Utility.GetPackages(dependency.Id, checkBoxPrerelease.Checked);
+                    IPackage depPackage = Utility.GetRangedPackageVersion(packages, dependency.VersionSpec);
+                    ShowConsole(string.Format("{0} -> {1}", package.GetFullName(), depPackage.GetFullName()));
+
+                    if (!Utility.IsPackageKnown(depPackage))
+                    {
+                        Utility.Packages.Add(depPackage);
+                        LoadDependencies(depPackage);
+                    }
+                }
+            }
+        }
+
+        private void DownloadPackages()
+        {
+            if (!Directory.Exists("download"))
+                Directory.CreateDirectory("download");
+
+            foreach (var package in Utility.Packages)
+            {
+                if (StopRequested)
+                    return;
+
+                string fileName = string.Format("{0}.{1}.nupkg", package.Id.ToLower(), package.Version);
+                if (File.Exists("download\\" + fileName))
+                {
+                    ShowConsole(string.Format("{0} already downloaded.", fileName));
+                    continue;
+                }
+
+                ShowConsole(string.Format("downloading {0}", fileName));
+                using (var client = new WebClient())
+                {
+                    var dsp = (DataServicePackage)package;
+                    client.DownloadFile(dsp.DownloadUrl, "download\\" + fileName);
+                }
+            }
+        }
+
+        SlidingBuffer<string> _consoleBuffer = new SlidingBuffer<string>(1000);
+
+        private void ShowConsole(string text)
+        {
+            Invoke(new MethodInvoker(
+                    delegate
+                    {
+                        _consoleBuffer.Add(text);
+
+                        textBoxActivity.Lines = _consoleBuffer.ToArray();
+                        textBoxActivity.Focus();
+                        textBoxActivity.SelectionStart = textBoxActivity.Text.Length;
+                        textBoxActivity.SelectionLength = 0;
+                        textBoxActivity.ScrollToCaret();
+                        textBoxActivity.Refresh();
+                    }
+                ));
+        }
+
+        private class Dependency
+        {
+            public string Id { get; set; }
+            public IVersionSpec VersionSpec { get; set; }
+        }
+   }
+}
